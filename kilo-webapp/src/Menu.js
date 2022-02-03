988639baf53bdbfdd29386be99387c054c02249c
George Willcox <georgewillcox@outlook.com>
2022-02-03 11:41:50 +0000

    Added getGameState function

--- kilo-webapp/src/Menu.js
+++ kilo-webapp/src/Menu.js
@@ -1,14 +1,34 @@
 import JoinGame from "./JoinGame";
+import httpRequest from './httpRequest';
+import React from 'react';
 
-function Menu(props) {
-  return (
-    <div className="d-grid gap-2">
-      <JoinGame
-        joinGame = {props.joinGame}
-      />
-      <button type="button" className="btn btn-primary">Button</button>
-    </div>
-  );
+class Menu extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  createGame() {
+    //const data = httpRequest("/api/create-game", "POST", JSON.stringify({name: "connect_4"}));
+    //const game_id = data.game_id;
+    const game_id = 1234
+    const game_id_element = document.getElementById("game-id-display")
+    game_id_element.innerHTML = `Game ID: ${game_id}`;
+    game_id_element.removeAttribute("hidden")
+    this.props.joinGame(game_id);
+  }
+
+  render() {
+    return (
+      <div className="d-grid gap-2">
+        <JoinGame
+          joinGame = {this.props.joinGame}
+        />
+        <button type="button" className="btn btn-primary" onClick={() => this.createGame()}>Create Game</button>
+        <h2 id="game-id-display" hidden>Game ID:</h2>
+        <button type=" button" className="btn btn-primary" onClick={() => this.props.getGameState()}>Get State</button>
+      </div>
+    );
+  }
 }
 
 export default Menu;
