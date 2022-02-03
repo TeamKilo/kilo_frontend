a7bab63b293fef51980a21b4719ac16adc913827
Oscar Hill <ogh22@cam.ac.uk>
2022-02-03 11:47:33 +0000

    Render connect 4 grid.

--- kilo-webapp/src/GameView.js
+++ kilo-webapp/src/GameView.js
@@ -1,9 +1,21 @@
-import httpRequest from './httpRequest';
+import Connect4Grid from "./Connect4Grid";
 
-function GameView() {
-    return (
-        <p>Game View</p>
+function GameView(props) {
+    const gameName = props.gameName;
+    const gameState = props.gameState;
+    let element = (
+        <div class="alert alert-danger" role="alert">
+            Error: unrecognised game.
+        </div>
     );
+
+    if (gameName === "connect_4") {
+        element = (
+            <Connect4Grid gridState={gameState.board} />
+        );
+    }
+
+    return element;
 }
 
 export default GameView;
