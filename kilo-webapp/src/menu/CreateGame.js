da79a4975496c9e91628ebca93845b3cb38baf99
George Willcox <georgewillcox@outlook.com>
2022-02-08 12:25:05 +0000

    Join game is now properly implmented

--- kilo-webapp/src/menu/CreateGame.js
+++ kilo-webapp/src/menu/CreateGame.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function CreateGame(props) {
-    const createdIDElement = props.createdID === null ? null : <div class="alert alert-success" role="alert">Created game with ID: {props.createdID}</div>;
+    const createdIDElement = props.createdID === null ? null : <div className="alert alert-success" role="alert">Created game with ID: {props.createdID}</div>;
     return (
         <React.Fragment>
             <button type="button" className="btn btn-primary" onClick={props.createGame}>Create Game</button>
