a204cb9418ef9e6f70864e75ccc586316e2fdac3
George Willcox <georgewillcox@outlook.com>
2022-02-08 12:45:52 +0000

    Added keys to grid elements to prevent React error

--- kilo-webapp/src/connect4/Connect4Grid.js
+++ kilo-webapp/src/connect4/Connect4Grid.js
@@ -1,12 +1,12 @@
 function Connect4Grid(props) {
     const gridState = props.gridState;
 
-    const tableItems = gridState.map((row) => {
-        const rowItems = row.map(cell => {
+    const tableItems = gridState.map((row, i) => {
+        const rowItems = row.map((cell, j) => {
             const colorClass = ["", "player1", "player2"];
-            return (<td className={"connect4cell " + colorClass[cell]}></td>);
+            return (<td key={j} className={"connect4cell " + colorClass[cell]}></td>);
         });
-        return (<tr>{rowItems}</tr>);
+        return (<tr key={i}>{rowItems}</tr>);
     });
 
     return (
