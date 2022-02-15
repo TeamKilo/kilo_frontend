26433823835dc105578e47b7cd39989624aa51b1
George Willcox <georgewillcox@outlook.com>
2022-02-15 11:50:34 +0000

    Site now scales properly for smaller resolutions

--- kilo-webapp/src/App.js
+++ kilo-webapp/src/App.js
@@ -1,19 +1,18 @@
 import Title from './Title';
 import Menu from './menu/Menu';
 import { Outlet } from "react-router-dom";
+import React from 'react';
 
-function App(props) {
-    return (
-        <div>
-            <div><Title /></div>
-            <div className="d-grid container">
-                <div className="row align-items-center" style={{ height: "90vh" }}>
-                    <div className="col-4">
-                        <Menu />
-                    </div>
-                    <div className="col-8 text-center">
-                        <Outlet />
-                    </div>
+function App() {
+    return (    
+        <div className="d-grid container">
+            <Title />
+            <div className="row align-items-center" id="surface" style={{ height: "90vh" }}>
+                <div className="col-lg-4" id="menu">
+                    <Menu />
+                </div>
+                <div className="col-lg-8 text-center" id="gameview">
+                    <Outlet />
                 </div>
             </div>
         </div>
