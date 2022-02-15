4f223483ee79271c19e263bd19d0060c8c9a5384
George Willcox <georgewillcox@outlook.com>
2022-02-15 13:59:16 +0000

    Changed grid to svg

--- kilo-webapp/src/connect4/Connect4Grid.js
+++ kilo-webapp/src/connect4/Connect4Grid.js
@@ -1,20 +1,57 @@
 function Connect4Grid(props) {
-    const gridState = props.gridState;
-
-    const tableItems = gridState.map((row, i) => {
-        const rowItems = row.map((cell, j) => {
-            const colorClass = ["", "player1", "player2"];
-            return (<td key={j} className={"connect4cell " + colorClass[cell]}></td>);
-        });
-        return (<tr key={i}>{rowItems}</tr>);
-    });
+    const size = 50;
+    const radius = size / 2 - 7;
+    const offset = 5;
+    const width = props.gridState[0].length;
+    const height = props.gridState.length;
+    const colorClass = ["", "player1", "player2"];
+    const playingPiece = (
+        <svg width={radius * 2} height={radius * 2}>
+            <circle cx={radius} cy={radius} r={radius} stroke="black" />
+        </svg>
+    );
 
     return (
-        <table className="table table-bordered" id="connect4table">
-            <tbody>
-                {tableItems}
-            </tbody>
-        </table>
+        <div style={{ position: "relative" }}>
+            <svg id="connect4table" width={size * width + offset * 2} height={size * (height + 1.5)}>
+                <path fillRule="evenodd" fill="blue" stroke="black" strokeWidth="3" strokeLinejoin="round"
+                    d={`M 5, ${size} h${size * width} v${size * height} h${-size * width} v${-size * height} h10` + props.gridState.map((row, y) => {
+                        return (
+                            row.map((_, x) => {
+                                return (
+                                    `
+                                        M ${size * x + size / 2 + offset} ${size * (y + 1) + size / 2}
+                                        m -${radius}, 0
+                                        a ${radius},${radius} 0 1,1 ${radius * 2},0
+                                        a ${radius},${radius} 0 1,1 ${-radius * 2},0
+                                    `
+                                );
+                            }).join(" ")
+                        );
+                    }).join(" ")}
+                />
+                {[...Array(7).keys()].map(x => 
+                    <text fontSize="18" x={size * x + radius + width} y={size * (height + 1.5) - 3}>{x + 1}</text>
+                )}
+            </svg>
+            <div style={{ position: "absolute", left: `calc(50% - ${size * width / 2 - offset - 2}px)`, top: `calc(50% - ${size * (height + 1.5) / 2 - size - offset - 1}px )`}}>
+                {props.gridState.map((row, y) => {
+                    return (
+                        row.map((cell, x) => {
+                            
+                            return (
+                                <div
+                                    className={"connect4cell " + colorClass[cell]}
+                                    style={{left: (x * size), top: (y * size)}}
+                                >
+                                    {cell ? playingPiece : ""}
+                                </div>
+                            );
+                        })
+                    )
+                })}
+            </div>
+        </div>
     );
 }
 
