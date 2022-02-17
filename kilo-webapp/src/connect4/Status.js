4abbe6e4150c961a24af51056f5514a5c5e5f497
Oscar Hill <ogh22@cam.ac.uk>
2022-02-17 11:04:53 +0000

    Fix another typo

--- kilo-webapp/src/connect4/Status.js
+++ kilo-webapp/src/connect4/Status.js
@@ -5,7 +5,7 @@ function Status(props) {
         return <p className="text-muted mb-0">Waiting for <span className="badge bg-secondary">{props.canMove[0]}</span> to move.</p>;
     } else if (props.status === "ended") {
         if (props.winners.length === 0) {
-            return <p className="text-muted mb-0">The game is over and no-one has won.</p>
+            return <p className="text-muted mb-0">The game is over and no one has won.</p>
         } else {
             return <p className="text-muted mb-0"><span className="badge bg-secondary">{props.winners[0]}</span> has won.</p>;
         }
