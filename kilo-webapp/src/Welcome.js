87c1a36548e8325aa5b3842dd2a851e0f137c0f7
Oscar Hill <ogh22@cam.ac.uk>
2022-02-22 10:53:53 +0000

    Update welcome message.

--- kilo-webapp/src/Welcome.js
+++ kilo-webapp/src/Welcome.js
@@ -5,7 +5,9 @@ function Welcome() {
             <div className="card-body">
                 <p>To view the state of an existing game, type its game ID and click View.</p>
                 <p>Click List Games to view a list of the games currently in the system.</p>
-                <p className="mb-0">To create a new game, click Create Game. You will then be taken to a visualisation of the new game which contains its ID.</p>
+                <p>To create a new game, click Create Game. You will then be taken to a visualisation of the new game which contains its ID.</p>
+                <p>If you are viewing a game which is waiting for players, you may join as a player and submit moves.</p>
+                <p className="mb-0">Click View API Docs to explore the API used to communicate with the server. This can be used to write your own bots.</p>
             </div>
         </div>
     );
