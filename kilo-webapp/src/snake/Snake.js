7c753dca62dcf45a9181afd259b67ae6726e3e97
Oscar Hill <ogh22@cam.ac.uk>
2022-03-01 21:00:30 +0000

    Move snake rendering into SnakeCanvas component so it mirrors connect 4 implementation.

--- kilo-webapp/src/snake/Snake.js
+++ kilo-webapp/src/snake/Snake.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React from "react";
+import SnakeCanvas from "./SnakeCanvas";
 import SnakeControls from "./SnakeControls";
 
 class Snake extends React.Component {
@@ -7,24 +8,6 @@ class Snake extends React.Component {
         super(props);
 
         this.submitMove = this.submitMove.bind(this);
-        this.colours = {
-            fruit: [249, 87, 56, 255],
-            snakes: [
-                [35, 201, 255, 255],
-                [255, 255, 255, 255],
-                [203, 99, 207, 255],
-                [0, 0, 0, 255]
-            ]
-        }
-        this.canvasWidth = 200;
-        this.canvasHeight = 200;
-    }
-
-    componentDidMount() {
-        this.canvas = document.getElementById("snake-canvas");
-        this.canvas.style.imageRendering = "pixelated";
-        this.ctx = this.canvas.getContext("2d");
-        this.drawCanvas();
     }
 
     submitMove(direction) {
@@ -34,42 +17,12 @@ class Snake extends React.Component {
         });
     }
 
-    drawCanvas() {
-        this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-        var imageData = this.ctx.getImageData(0, 0, this.canvasWidth, this.canvasHeight);
-
-        this.props.gameState.payload.fruits.forEach(position => {
-            this.setColour(imageData, position, this.colours.fruit);
-        });
-        console.log(this.props.gameState.payload.players);
-        this.props.gameState.players.forEach((player, index) => {
-            if (player in this.props.gameState.payload.players) {
-                this.props.gameState.payload.players[player].forEach(position => {
-                    this.setColour(imageData, position, this.colours.snakes[index]);
-                });
-            }
-        });
-
-        this.ctx.putImageData(imageData, 0, 0);
-    }
-
-    setColour(imageData, position, colour) {
-        var offset = ((this.canvasHeight - (position.y + this.canvasHeight / 2)) * imageData.width + (position.x + this.canvasWidth / 2)) * 4;
-        colour.forEach((value, index) => imageData.data[offset + index] = value);
-    }
-
     render() {
-        if (this.ctx !== undefined) {
-            this.drawCanvas()
-        }
-
         return (
-            <React.Fragment>
-                <div id="canvas-container">
-                    <canvas id="snake-canvas" width={this.canvasWidth} height={this.canvasHeight} />
-                </div>
+            <>
+                <SnakeCanvas players={this.props.gameState.payload.players} fruits={this.props.gameState.payload.fruits} />
                 {this.props.sessionID === undefined ? null : <SnakeControls submitMove={this.submitMove} />}
-            </ React.Fragment>
+            </>
         );
     }
 }
---
+++ kilo-webapp/src/snake/SnakeCanvas.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+class SnakeCanvas extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.colours = {
+            fruit: [249, 87, 56, 255],
+            snakes: [
+                [35, 201, 255, 255],
+                [255, 255, 255, 255],
+                [203, 99, 207, 255],
+                [0, 0, 0, 255]
+            ]
+        }
+        this.canvasWidth = 200;
+        this.canvasHeight = 200;
+    }
+
+    componentDidMount() {
+        this.canvas = document.getElementById("snake-canvas");
+        this.canvas.style.imageRendering = "pixelated";
+        this.ctx = this.canvas.getContext("2d");
+        this.drawCanvas();
+    }
+
+    drawCanvas() {
+        this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+        var imageData = this.ctx.getImageData(0, 0, this.canvasWidth, this.canvasHeight);
+
+        this.props.fruits.forEach(position => {
+            this.setColour(imageData, position, this.colours.fruit);
+        });
+        console.log(this.props.players);
+        this.props.players.forEach((player, index) => {
+            if (player in this.props.players) {
+                this.props.players[player].forEach(position => {
+                    this.setColour(imageData, position, this.colours.snakes[index]);
+                });
+            }
+        });
+
+        this.ctx.putImageData(imageData, 0, 0);
+    }
+
+    setColour(imageData, position, colour) {
+        var offset = ((this.canvasHeight - (position.y + this.canvasHeight / 2)) * imageData.width + (position.x + this.canvasWidth / 2)) * 4;
+        colour.forEach((value, index) => imageData.data[offset + index] = value);
+    }
+
+    render() {
+        if (this.ctx !== undefined) {
+            this.drawCanvas()
+        }
+
+        return (
+            <div id="canvas-container">
+                <canvas id="snake-canvas" width={this.canvasWidth} height={this.canvasHeight} />
+            </div>
+        );
+    }
+}
+
+export default SnakeCanvas;
