76c7490e41401082565f51c2e126f828c2728bf4
Oscar Hill <ogh22@cam.ac.uk>
2022-03-03 13:21:23 +0000

    Stop scrolling when arrow keys pressed to submit moves.

--- kilo-webapp/src/snake/SnakeControls.js
+++ kilo-webapp/src/snake/SnakeControls.js
@@ -14,6 +14,7 @@ class SnakeControls extends React.Component {
             } else if (e.code === "ArrowRight") {
                 props.submitMove("right");
             }
+            e.preventDefault();
         };
     }
 
