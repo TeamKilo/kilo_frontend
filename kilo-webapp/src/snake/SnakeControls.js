046c13100fb3c69d395e1fda110c0a582b123f4a
Oscar Hill <ogh22@cam.ac.uk>
2022-03-03 12:26:35 +0000

    Add keyboard controls.

--- kilo-webapp/src/snake/SnakeControls.js
+++ kilo-webapp/src/snake/SnakeControls.js
@@ -1,15 +1,43 @@
-function SnakeControls(props) {
-    return (
-        <>
-            <button type="button" className="btn btn-primary" style={{width: "2.5em"}} onClick={() => props.submitMove("up")}>↑</button>
-            <br />
-            <button type="button" className="btn btn-primary" style={{width: "2.5em", marginRight: "1.25em"}} onClick={() => props.submitMove("left")}>←</button>
-            <button type="button" className="btn btn-primary" style={{width: "2.5em", marginLeft: "1.25em"}} onClick={() => props.submitMove("right")}>→</button>
-            <br />
-            <button type="button" className="btn btn-primary mb-4" style={{width: "2.5em"}} onClick={() => props.submitMove("down")}>↓</button>
-        
-        </>
-    );
+import React from "react";
+
+class SnakeControls extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.controller = new AbortController();
+    }
+
+    componentDidMount() {
+        document.addEventListener("keydown", (e) => {
+            if (e.code === "ArrowUp") {
+                this.props.submitMove("up");
+            } else if (e.code === "ArrowDown") {
+                this.props.submitMove("down");
+            } else if (e.code === "ArrowLeft") {
+                this.props.submitMove("left");
+            } else if (e.code === "ArrowRight") {
+                this.props.submitMove("right");
+            }
+        }, {signal: this.controller.signal});
+    }
+
+    componentWillUnmount() {
+        this.controller.abort();
+    }
+
+    render() {
+        return (
+            <>
+                <button type="button" className="btn btn-primary" style={{width: "2.5em"}} onClick={() => this.props.submitMove("up")}>↑</button>
+                <br />
+                <button type="button" className="btn btn-primary" style={{width: "2.5em", marginRight: "1.25em"}} onClick={() => this.props.submitMove("left")}>←</button>
+                <button type="button" className="btn btn-primary" style={{width: "2.5em", marginLeft: "1.25em"}} onClick={() => this.props.submitMove("right")}>→</button>
+                <br />
+                <button type="button" className="btn btn-primary mb-4" style={{width: "2.5em"}} onClick={() => this.props.submitMove("down")}>↓</button>
+            
+            </>
+        );
+    }
 }
 
 export default SnakeControls;
